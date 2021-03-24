196d18d7a38c72b650ee352707f4be54aa56d9ae
Nazia R <nazia.huma@gmail.com>
2021-03-24 16:02:58 +0000

    set the timer to tik for 60 seconds

--- js/script.js
+++ js/script.js
@@ -1,9 +1,20 @@
-startQuiz() {
+const timerEl = document.getElementById("timer");
+const introSection = document.getElementById("intro-section");
+const startBtn = document.getElementById("start-button");
 
-}
+let secondsLeft = 60;
 
-//remove intro-section div
-//start timer
-//setinterval (fun,1000) this fun will be cont timertik 
+const setTime = function () {
+  const callBack = function () {
+    if (secondsLeft > 0) {
+      secondsLeft = secondsLeft - 1;
+    }
 
-const timeValue = 60
+    timerEl.textContent = secondsLeft;
+  };
+  const timerInterval = setInterval(callBack, 1000);
+};
+
+//setTime();
+
+startBtn.addEventListener("click", setTime);
